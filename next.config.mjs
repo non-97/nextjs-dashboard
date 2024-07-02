b09c879518a657506d7b8e1639904222a322e942
non-97 <nonmonaka07@gmail.com>
2024-07-02 17:08:05 +0900

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
+import withExportImages from "next-export-optimize-images";
+
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {};
 
-export default nextConfig;
+if (process.env.NEXT_EXPORT === "true") {
+  nextConfig.output = "export";
+}
+
+export default withExportImages(nextConfig);

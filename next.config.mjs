fd76ca60e3149952db729a95b634703e7a0b9eeb
non-97 <nonmonaka07@gmail.com>
2024-07-03 09:32:21 +0900

    環境変数で条件分岐をする必要がなかったためbuildに集約

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,8 @@ import withExportImages from "next-export-optimize-images";
 
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
-
-if (process.env.NEXT_EXPORT === "true") {
-  nextConfig.output = "export";
-}
+const nextConfig = {
+  output : "export"
+};
 
 export default withExportImages(nextConfig);
